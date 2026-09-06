f5e282f125bcf8a092d7bb1d52b106fcef963391
Jasonnguyn <jasonnguyen7506@gmail.com>
2026-09-06 00:04:28 -0700

    Bulk sync: 98 submissions

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+        
+
+    
+        # Space is O(N) we visit each node once
+        # Time complexity is O(N) worst case its skewed and visit every node
+        # Balanced is O(LOGN) though 
